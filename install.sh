eb7e43175686a344017ac1d2f9d8442ce7204530
elgs <elgs1980@hotmail.com>
2015-12-10 12:47:19 -0800

    Update.

--- install.sh
+++ install.sh
@@ -4,11 +4,13 @@ server_id="$1"
 app_id="5f04e621-b137-4fde-81be-b5d16b5c9160"
 token="a1d05b7e-c025-4ca1-ae40-5e08c3aaf97c"
 
+nd_server=`curl -s0 https://netdata.io:2015/sys/get_server`
+
 server_data=`curl -s -0 -X GET \
--H "token: 7de7ae97-ea02-47df-bf61-05400cc96c43" \
+-H "token: $token" \
 -H "app_id: $app_id" \
 -F "params=$server_id" \
-"https://netdata.io:2015/api/query_server?query=1"`
+"https://$nd_server/api/query_server?query=1"`
 
 if [[ ! -f "/usr/bin/jq" ]] || [[ ! -f "/usr/bin/curl"  ]]; then
   apt-get update && apt-get install -y jq curl
@@ -19,6 +21,8 @@ if [[ "$server_profile" == "null" ]]; then
   >&2 echo "Failed to get server infomation. Possibly invalid server id."
   exit 1
 fi
+
+version=$(echo "$server_data" | jq -r '.data[0].VERSION')
 
 mount_point=$(echo "$server_profile" | jq -r '.mount_point')
 process=$(echo "$server_profile" | jq -r '.process')
@@ -32,7 +36,6 @@ warn_rx=$(echo "$server_profile" | jq -r '.warn_rx')
 warn_tx=$(echo "$server_profile" | jq -r '.warn_tx')
 warn_rx_rate=$(echo "$server_profile" | jq -r '.warn_rx_rate')
 warn_tx_rate=$(echo "$server_profile" | jq -r '.warn_tx_rate')
-update_interval=$(echo "$server_profile" | jq -r '.update_interval')
 
 if [[ "$update_interval" == "null" ]]; then
 	update_interval=1
@@ -47,6 +50,8 @@ chmod +x "$tmp_path"
 sed -i s/__app_id__/${app_id}/g "$tmp_path"
 sed -i s/__server_id__/${server_id}/g "$tmp_path"
 sed -i s/__token__/${token}/g "$tmp_path"
+sed -i s/__nd_server__/${nd_server}/g "$tmp_path"
+sed -i s/__version__/${version}/g "$tmp_path"
 sed -i s/__mount_point__/$(echo ${mount_point} | sed -e 's/\\/\\\\/g' -e 's/\//\\\//g' -e 's/&/\\\&/g')/g "$tmp_path"
 sed -i s/__process__/${process}/g "$tmp_path"
 sed -i s/__nic__/${nic}/g "$tmp_path"
